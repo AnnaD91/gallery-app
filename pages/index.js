05ee801ccbd7695838881fb71bd1a9cfecd63303
Anna Dick <anna_dick@outlook.de>
2023-09-01 16:38:41 +0200

    fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
+import useSWR from "swr";
+import { useState, useEffect } from "react";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
 export default function HomePage() {
+  /* const { data, isLoading } = useSWR(
+    page ? `https://example-apis.vercel.app/api/art` : null,
+    { revalidateIfStale: false }
+  ); */
+  const { data } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
+
+  /*   if (isLoading || !data) {
+    return "Loading...";
+  } */
+
+  const pieces = data;
+  console.log(data);
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <h1>Gallery App</h1>
+      <div></div>
+    </>
+  );
+}
+
+function Artpieces({ pieces }) {
+  return (
+    <>
+      <ul key={pieces.slug}>
+        <li>
+          image={pieces.image}
+          title={pieces.name}
+          artist={pieces.artist}
+        </li>
+      </ul>
+    </>
   );
 }
